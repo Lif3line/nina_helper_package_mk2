a4c518195121c59ce4094db3ce9b85f6ad6098ac
Adam Hartwell <adamhartwell2@gmail.com>
2017-07-05 13:32:24 +0100

    Updates for PyPI

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 
 setup(name='nina_helper',
-      version='2',
+      version='2.0',
       description='Utiltiy functions for workign on the NinaPro Databases (1 & 2)',
-      url='https://github.com/Lif3line',
       author='Lif3line',
       author_email='adamhartwell2@gmail.com',
       license='MIT',
       packages=find_packages(),
-      zip_safe=False)
+      url='https://github.com/Lif3line/nina_helper_package_mk2',  # use the URL to the github repo
+      download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.0.tar.gz',  # Hack from github address
+      keywords='ninapro emg')
