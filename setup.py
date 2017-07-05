ad579b09e691811a39714f816721bba615dcc651
Adam Hartwell <adamhartwell2@gmail.com>
2017-07-05 15:02:20 +0100

    Use local upload for release

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from setuptools import setup, find_packages
 
 
 setup(name='nina_helper',
-      version='2.1',
+      version='2.2',
       description='Utiltiy functions for workign on the NinaPro Databases (1 & 2)',
       author='Lif3line',
       author_email='adamhartwell2@gmail.com',
       license='MIT',
       packages=find_packages(),
       url='https://github.com/Lif3line/nina_helper_package_mk2',  # use the URL to the github repo
-      download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.1.tar.gz',  # Hack github address
+      # download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.2.tar.gz',  # Hack github address
       install_requires=[
-          'os',
           'scipy',
           'sklearn',
-          'itertools',
           'numpy'
       ],
       keywords='ninapro emg')
