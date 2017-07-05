6f5ff2bad49c3374ef72ffa9d8b0ffbb659666e6
Adam Hartwell <adamhartwell2@gmail.com>
2017-07-05 14:52:42 +0100

    Add install_requires

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@ from setuptools import setup, find_packages
 
 
 setup(name='nina_helper',
-      version='2.0',
+      version='2.1',
       description='Utiltiy functions for workign on the NinaPro Databases (1 & 2)',
       author='Lif3line',
       author_email='adamhartwell2@gmail.com',
       license='MIT',
       packages=find_packages(),
       url='https://github.com/Lif3line/nina_helper_package_mk2',  # use the URL to the github repo
-      download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.0.tar.gz',  # Hack from github address
+      download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.1.tar.gz',  # Hack github address
+      install_requires=[
+          'os',
+          'scipy',
+          'sklearn',
+          'itertools',
+          'numpy'
+      ],
       keywords='ninapro emg')
